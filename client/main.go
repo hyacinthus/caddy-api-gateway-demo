18b6c4306bcae1bf757d5fb53c784778103d72da
wangrh@99 <wangrh@sdkeji.com>
2017-03-17 12:03:36 +0800

    let test client use docker

--- client/main.go
+++ client/main.go
@@ -4,28 +4,43 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	fin := make(chan bool)
+	// dist count
+	res := make(map[string]int)
+	// start concurrent request
+	fin := make(chan string)
 	for i := 0; i < 30; i++ {
 		go req(fin)
 	}
 	for i := 0; i < 30; i++ {
-		<-fin
+		resp := <-fin
+		_, ok := res[resp]
+		if ok {
+			res[resp]++
+		} else {
+			res[resp] = 1
+		}
+	}
+	log.Printf("statistics:\n%+v", res)
+	if len(res) <= 1 {
+		os.Exit(1)
 	}
 }
 
-func req(fin chan bool) {
-	res, err := http.Get("http://127.0.0.1:2015")
+func req(fin chan string) {
+	// docker0 interface is 172.10.0.1
+	res, err := http.Get("http://172.17.0.1:2015")
 	if err != nil {
 		log.Fatal(err)
 	}
-	robots, err := ioutil.ReadAll(res.Body)
+	content, err := ioutil.ReadAll(res.Body)
 	res.Body.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("%s", robots)
-	fin <- true
+	log.Printf("%s", content)
+	fin <- string(content)
 }
