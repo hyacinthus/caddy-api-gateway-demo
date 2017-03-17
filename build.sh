a79d095bd436bdc83e8684703fd15498c50c0fc5
wangrh@99 <wangrh@sdkeji.com>
2017-03-17 16:09:00 +0800

    put build cmd in shell

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
 docker run --rm -v ${PWD}:/go/src/app -w /go/src/app golang:alpine go build -v
+docker build -f Dockerfile.app -t muninn/caddy-microservice:app .
+docker build -f Dockerfile.gateway -t muninn/caddy-microservice:gateway .
--- client/build.sh
+++ client/build.sh
@@ -1,1 +1,2 @@
 docker run --rm -v ${PWD}:/go/src/client -w /go/src/client golang:alpine go build -v
+docker build -f Dockerfile.client -t muninn/caddy-microservice:client .
